cfd2567bebd19ff384fe07432f99b0aaeb2387a0
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-05-24 21:01:20 +0200

    chore: Update Prisma provider to use PostgreSQL and add driverAdapters preview feature

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import { automaticCheck } from "./Discord_Bot/AutomaticMessages/automaticCheck.js";
-import { startBot } from "./Discord_Bot/bot.js";
+import { PrismaPg } from '@prisma/adapter-pg';
+import { automaticCheck } from './Discord_Bot/AutomaticMessages/automaticCheck.js';
+import { startBot } from './Discord_Bot/bot.js';
+import { PrismaClient } from '@prisma/client';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+console.log(`Connecting to database at ${connectionString}`);
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter });
 
 export const client = await startBot();
 
