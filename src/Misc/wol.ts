6feb45318302ab3f9f8b6262acb9ababeb42d5c7
Emil Deßloch <117310683+Limonadeundco@users.noreply.github.com>
2024-06-07 06:39:03 +0000

    chore: Remove unused wake_on_lan dependency and code

--- src/Misc/wol.ts
+++ src/Misc/wol.ts
@@ -1,5 +1,4 @@
 import { NodeSSH } from 'node-ssh';
-import wol from 'wake_on_lan';
 
 export async function sendWakeOnLanPacket(macAddress: string, sshHost: string, sshPort: number, sshUser: string, sshKey: string) {
     const ssh = new NodeSSH();
@@ -21,16 +20,6 @@ export async function sendWakeOnLanPacket(macAddress: string, sshHost: string, s
             throw new Error(`Error executing wakeonlan command: ${result.stderr}`);
         }
 
-        // If the command was successful, send a WOL packet as well
-        return new Promise<void>((resolve, reject) => {
-            wol.wake(macAddress, (error) => {
-                if (error) {
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
-        });
     } catch (error) {
         throw error;
     } finally {
