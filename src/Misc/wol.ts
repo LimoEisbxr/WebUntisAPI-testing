79e65117cde98915dcd1815387fa9168b7c4e2d8
Emil Deßloch <emildess0303@gymhelm.onmicrosoft.com>
2024-06-07 21:55:19 +0200

    chore: Update npm dependencies and add @types/ssh2-sftp-client

--- src/Misc/wol.ts
+++ src/Misc/wol.ts
@@ -1,28 +1,45 @@
-import { NodeSSH } from 'node-ssh';
+import { Client } from 'ssh2';
 
-export async function sendWakeOnLanPacket(macAddress: string, sshHost: string, sshPort: number, sshUser: string, sshKey: string) {
-    const ssh = new NodeSSH();
+export async function sendWakeOnLanPacket(
+    macAddress: string,
+    sshHost: string,
+    sshPort: number,
+    sshUser: string,
+    sshKey: string
+) {
+    const conn = new Client();
 
     try {
         // Connect to the SSH server
-        await ssh.connect({
+        await conn.connect({
             host: sshHost,
             port: sshPort,
             username: sshUser,
-            privateKey: sshKey
+            privateKey: sshKey,
         });
 
         // Execute the wakeonlan command
         const wakeCommand = `wakeonlan ${macAddress}`;
-        const result = await ssh.execCommand(wakeCommand);
-
-        if (result.stderr) {
-            throw new Error(`Error executing wakeonlan command: ${result.stderr}`);
-        }
-
+        conn.exec(wakeCommand, (err, stream) => {
+            if (err) throw err;
+            stream
+                .on('close', (code: number, signal: string) => {
+                    console.log(
+                        'Stream :: close :: code: ' +
+                            code +
+                            ', signal: ' +
+                            signal
+                    );
+                    conn.end();
+                })
+                .on('data', (data: any) => {
+                    console.log('STDOUT: ' + data);
+                })
+                .stderr.on('data', (data: any) => {
+                    console.error('STDERR: ' + data);
+                });
+        });
     } catch (error) {
         throw error;
-    } finally {
-        ssh.dispose();
     }
 }
